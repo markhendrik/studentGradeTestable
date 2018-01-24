755850e29c6a4e47c17dcfd0292e7f5bdcbc3de8
Mark <mark.maeste@gmail.com>
2018-01-24 13:34:47 +0200

    Grade Entry enum added.

--- src/persona/GradeEntry.java
+++ src/persona/GradeEntry.java
@@ -1,8 +1,15 @@
 package persona;
 
+
+
 public class GradeEntry {
 	private Integer grade;
-	private String evalation;
+	private Evaluation evalation;
+	
+	public GradeEntry(Integer grade, Evaluation evalation) {
+		this.grade = grade;
+		this.evalation = evalation;
+	}
 	
 	public Integer getGrade() {
 		return grade;
@@ -10,10 +17,21 @@ public class GradeEntry {
 	public void setGrade(Integer grade) {
 		this.grade = grade;
 	}
-	public String getEvalation() {
+	public Evaluation getEvalation() {
 		return evalation;
 	}
-	public void setEvalation(String evalation) {
+	public void setEvalation(Evaluation evalation) {
 		this.evalation = evalation;
-	}	
+	}
+	
+	public enum Evaluation {
+		UUESTI_TEGEMISEL, // 1, 2, 3
+		MITTERAHULDAV, // 1, 2
+		RAHULDAV, // 3,4
+		HEA, // 4
+		VÄGA_HEA, // 4, 5
+		SUUREPÄRANE, // 5
+		VÕIKS_PAREM_OLLA, // 1, 2, 3, 4
+		PARIM // 5
+	}
 }
